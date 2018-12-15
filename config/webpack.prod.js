a042e08d6e3d4d89e62e0a726699084dd3e79c5a
milad <milad.vafaeifard@gmail.com>
2018-12-15 18:30:56 +0100

    Add plugins and externals in production mode

--- config/webpack.prod.js
+++ config/webpack.prod.js
@@ -1,8 +1,18 @@
 const webpackMerge = require('webpack-merge')
+const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer')
 const baseConfig = require('./webpack.base')
 
 const config = {
-    mode: 'production'
+    mode: 'production',
+    plugins: [new BundleAnalyzerPlugin({
+        analyzerMode: 'static',
+        openAnalyzer: false,
+        reportFilename: 'bundle_sizes.html'
+    })],
+    externals: {
+        'react': 'React',
+        'react-dom' : 'ReactDOM'
+    }
 }
 
-module.exports = webpackMerge(baseConfig, config);
+module.exports = webpackMerge(baseConfig, config)
