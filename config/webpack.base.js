d4aea4cd49689c3aca5607bd71e367879ca03e7a
milad <milad.vafaeifard@gmail.com>
2018-12-15 18:48:07 +0100

    Install polyfill and browserlist support

--- config/webpack.base.js
+++ config/webpack.base.js
@@ -7,36 +7,44 @@ const template = path.resolve(__dirname, '../src/index.html')
 const IGNORE_NODE_MODULES = /node_modules/;
 
 module.exports = {
-    entry,
-    output: {
-        path: outputPath,
-        filename: 'app.bundle.js'
-    },
-    module: {
-        rules: [
+	entry,
+	output: {
+		path: outputPath,
+		filename: 'app.bundle.js'
+	},
+	module: {
+		rules: [
             {
-                test: /\.js$/,
-                loader: 'babel-loader',
-                exclude: IGNORE_NODE_MODULES,
-                options: {
-                    presets: [
-                        '@babel/preset-env', 
-                        '@babel/preset-react'
-                    ],
-                    plugins: [
-                        'react-hot-loader/babel',
-                        '@babel/plugin-proposal-class-properties'
-                    ]
-                }
-            },
-            {
-                test: /\.css$/,
-                use: ['style-loader', 'css-loader'],
-                exclude: IGNORE_NODE_MODULES,
-            }
-        ]
-    },
-    plugins: [new HtmlWebpackPlugin({
-        template
-    })]
+				test: /\.js$/,
+				loader: 'babel-loader',
+				exclude: IGNORE_NODE_MODULES,
+				options: {
+					presets: [
+						'@babel/preset-env', {
+							targets: [
+								'last 2 versions',
+								'not dead',
+								'not < 2%',
+								'not ie 11', // take out if we need to support IE 11
+							],
+							useBuiltIns: 'entry'
+						},
+						'@babel/preset-react'
+					],
+					plugins: [
+						'react-hot-loader/babel',
+						'@babel/plugin-proposal-class-properties'
+					]
+				}
+			},
+			{
+				test: /\.css$/,
+				use: ['style-loader', 'css-loader'],
+				exclude: IGNORE_NODE_MODULES,
+			}
+		]
+	},
+	plugins: [new HtmlWebpackPlugin({
+		template
+	})]
 }
