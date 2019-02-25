6010c19a69e1026996b0382bb33224e85e0cf078
milad <milad.vafaeifard@gmail.com>
2019-02-25 11:32:17 +0100

    Add counter for experimenting test

--- src/app/App.js
+++ src/app/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react'
 import { hot } from 'react-hot-loader'
+import { CounterComponent } from './components/Counter/'
 
 class App extends Component {
   render() {
     return (
       <div>
         <h1>Hello World</h1>
+        <CounterComponent title="Timer" />
       </div>
     )
   }
--- src/app/components/Counter/Counter.component.js
+++ src/app/components/Counter/Counter.component.js
@@ -1,19 +1,44 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 
-class Counter extends Component {
-	render() {
-		return (
-			<div>
-				<h1>{this.props.title}</h1>
-				Counter component rendered
-			</div>
-		)
-	}
+class CounterComponent extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      count: 0
+    }
+  }
+
+  increaseHandle = () => {
+    this.setState(state => ({
+      count: state.count + 1
+    }))
+  }
+
+  decreaseHandle = () => {
+    this.setState(state => ({
+      count: state.count > 0 ? state.count - 1 : 0
+    }))
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>{this.props.title}</h1>
+        <p className="output-count">{this.state.count}</p>
+        <button id="increaseButton" onClick={this.increaseHandle}>
+          UP
+        </button>
+        <button id="decreaseButton" onClick={this.decreaseHandle}>
+          DOWN
+        </button>
+      </div>
+    )
+  }
 }
 
-Counter.propTypes = {
-	title: PropTypes.string
+CounterComponent.propTypes = {
+  title: PropTypes.string
 }
 
-export default Counter
+export default CounterComponent
