f5191f2408f2012b1e7e8fb1a3ef6779b423bfaf
milad <milad.vafaeifard@gmail.com>
2019-02-25 10:50:58 +0100

    Add packages: enzyme, enzyme-adapter and others

--- testSetup.js
+++ testSetup.js
@@ -1,2 +1,7 @@
 import 'jest-dom/extend-expect'
 import 'react-testing-library/cleanup-after-each'
+
+// Enzyme configuration
+import Enzyme from 'enzyme'
+import Adapter from 'enzyme-adapter-react-16'
+Enzyme.configure({ adapter: new Adapter() })
